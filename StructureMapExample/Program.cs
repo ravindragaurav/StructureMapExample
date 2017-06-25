727b17d0614bf127f058ab0eafd498e855589fa4
Gaurav Ravindra <ravindra_gaurav@yahoo.com>
2017-06-25 15:38:37 +0100

    implementeed structureMap

--- StructureMapExample/Program.cs
+++ StructureMapExample/Program.cs
@@ -1,4 +1,5 @@
-﻿using StructureMapExample.Classes;
+﻿using StructureMap;
+using StructureMapExample.Classes;
 using System;
 
 namespace StructureMapExample
@@ -7,10 +8,19 @@ namespace StructureMapExample
     {
         static void Main(string[] args)
         {
-            ITasks tasks = new Tasks();
-            Employee e = new Employee(tasks);
-            e.PerformTask("Coding");
+            //ITasks tasks = new Tasks();
+            //Employee e = new Employee(tasks);
+            //e.PerformTask("Coding");
+
+            var container = new Container();
+            container.Configure(x => x.For<ITasks>().Use<Tasks>().Named("Normal Tasks"));
+            container.Configure(x => x.For<ITasks>().Use<Duties>().Named("Compulsory Tasks"));
+
+            var shopper = container.GetInstance<Employee>();
+            shopper.PerformTask("test"); //should be compulsory since registered last and using without name
             Console.ReadLine();
+
+            //cant get named instance, have to pass in to Employee constructor to somehow know which one to use.
         }
     }
 }
